26e2a45c2b48ebfc4c6a3c37a8c49cdf9b3d6d0a
Gianluca Sudano <sudanogianluca@gmail.com>
2024-02-10 20:54:32 +0100

    removed commented code

--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,5 +1,7 @@
-import '@testing-library/jest-dom/vitest'
-import { afterAll, afterEach, beforeAll, expect } from 'vitest';
+/* eslint-disable import/no-extraneous-dependencies */
+
+import '@testing-library/jest-dom/vitest';
+import { afterAll, afterEach, beforeAll } from 'vitest';
 import { server } from './mocks/server';
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
@@ -8,5 +10,3 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 // Clean up after the tests are finished.
 afterAll(() => server.close());
-
-// expect.extend(matchers);
